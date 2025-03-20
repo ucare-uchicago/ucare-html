28292bcfddf6d352e7d932cd86f88d5ffee2045c
Ray Andrew <rs@rs.ht>
2025-03-20 15:23:14 -0500

    ray: update refresh-stage-dir

--- refresh-stage-dir.sh
+++ refresh-stage-dir.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-git push
+# git push
 git status
 
 echo "  Refreshing"
 
 
-ssh -A linux2.cs.uchicago.edu \
-    'git config --global --add safe.directory /stage/ucare/html'
+# ssh -A linux2.cs.uchicago.edu \
+#     'git config --global --add safe.directory /stage/ucare/html'
 
-ssh -o StrictHostKeyChecking=no -A linux2.cs.uchicago.edu \
-	'cd /stage/ucare/html ; GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git pull ; sh change-permission.sh' >/tmp/change-permission.txt
+# ssh -o StrictHostKeyChecking=no -A linux2.cs.uchicago.edu \
+# 	'cd /stage/ucare/html ; GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git pull ; sh change-permission.sh' >/tmp/change-permission.txt
+
+rsync -Parv --exclude ".git" ./* linux2.cs.uchicago.edu:/stage/ucare/html
+ssh -o StrictHostKeyChecking=no -A linux2.cs.uchicago.edu 'cd /stage/ucare/html ; sh change-permission.sh' >/tmp/change-permission.txt
 echo ""
 cat /tmp/change-permission.txt
 echo "  "
