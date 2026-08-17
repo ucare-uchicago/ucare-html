f2afe13894dbe6ebd967a5390ee826deda1d5265
ruidan-li <ruidan.li.lir@gmail.com>
2026-08-17 13:40:48 -0500

    li: update refresh script

--- refresh-stage-dir.sh
+++ refresh-stage-dir.sh
@@ -12,8 +12,8 @@ echo "  Refreshing"
 # ssh -o StrictHostKeyChecking=no -A linux2.cs.uchicago.edu \
 # 	'cd /stage/ucare/html ; GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git pull ; sh change-permission.sh' >/tmp/change-permission.txt
 
-rsync -Parv --exclude ".git" ./* ruidanli@linux2.cs.uchicago.edu:/stage/ucare/html
-ssh -o StrictHostKeyChecking=no -A ruidanli@linux2.cs.uchicago.edu 'cd /stage/ucare/html ; sh change-permission.sh' >/tmp/change-permission.txt
+rsync -Parv --exclude ".git" ./* linux2.cs.uchicago.edu:/stage/ucare/html
+ssh -o StrictHostKeyChecking=no -A linux2.cs.uchicago.edu 'cd /stage/ucare/html ; sh change-permission.sh' >/tmp/change-permission.txt
 echo ""
 cat /tmp/change-permission.txt
 echo "  "
